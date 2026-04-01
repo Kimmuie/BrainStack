ed4b37c734e530b09b6538a824d09b333e3bb786
Kimmuie <pranchayutnetsawang@gmail.com>
2026-04-01 17:07:46 +0700

    TextField for Editing Username

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,9 @@ const Navbar = () => {
     const userSettingRef = useRef(null);
     const [isLogin, setIsLogin] = useState(false);
     const [isUserOpen, setIsUserOpen] = useState(false);
+    const [isEditProfile, setIsEditProfile] = useState(false);
+    const [isUsername, setIsUsername] = useState("Firstname Lastname");
+    const [isUserEmail, setIsUserEmail] = useState("example@example@gmail.com");
 
     return (
         <nav className="fixed z-50 bg-Secondary w-full h-20 flex items-center justify-between px-8 py-4 border-b-2 border-Primary">
@@ -50,13 +53,24 @@ const Navbar = () => {
             >
                 <div className="flex items-center just gap-3 p-4 border-b border-Primary/20">
                     <div className="overflow-hidden">
+                        {isEditProfile ? (
                         <p className="text-Secondary text-sm font-bold truncate">Lorem Ipsum</p>
+                        ) : (
+                        <div className="flex flex-row w-full rounded-full border-2 bg-Darker-Secondary border-Primary py-1 px-1">
+                            <input type="text" placeholder="Username" className="text-Primary w-full px-2 focus:outline-none focus:ring-0 focus:border-transparent" />
+                            <button className="font-bold ml-4 bg-Primary text-Secondary px-3 py-1 rounded-full border-2 border-Primary hover:bg-Darker-Primary cursor-pointer transition-colors duration-300">
+                                <img src="/img/icon_check_dark.svg" alt="Dark mode" className="w-6 h-6" />
+                            </button>
+                        </div>
+                        )}
                         <p className="text-Secondary/60 text-xs truncate">lorem@example.com</p>
                     </div>
                 </div>
                 <div className="p-2">
                     {/* Name Changer - งานคิม */}
-                    <button className="w-full flex items-center gap-3 px-3 py-2.5 rounded-lg hover:bg-Secondary/10 transition-colors text-sm text-Secondary text-left cursor-pointer">
+                    <button 
+                        onClick={() => setIsEditProfile(prev => !prev)}
+                        className="w-full flex items-center gap-3 px-3 py-2.5 rounded-lg hover:bg-Secondary/10 transition-colors text-sm text-Secondary text-left cursor-pointer">
                         <img src="/img/icon_edit_dark.svg" alt="Edit" className="w-4 h-4" />
                         Edit profile
                     </button>
