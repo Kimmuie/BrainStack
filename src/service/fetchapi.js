c9272f474bbccb779d657a90bd66475a793a715e
Kimmuie <pranchayutnetsawang@gmail.com>
2026-04-22 15:16:49 +0700

    Redeploy

--- src/service/fetchapi.js
+++ src/service/fetchapi.js
@@ -1,5 +1,5 @@
 export const fetchAPI = async (endpoint, method = 'GET', body = null) => {
-    const base_url = `${import.meta.env.VITE_API_URL}/ฺBrainstack`;//redeploy
+    const base_url = `${import.meta.env.VITE_API_URL}/Brainstack`;
 
     try {   
         const options = {
